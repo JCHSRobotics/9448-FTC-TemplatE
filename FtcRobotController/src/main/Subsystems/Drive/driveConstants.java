47acfb83e5cdefdcdfc46b445904cd60a58e2166
TheDeadChicken <mishi.doron@gmail.com>
2026-02-27 12:55:54 -0800

    do setup

---
+++ FtcRobotController/src/main/Subsystems/Drive/driveConstants.java
@@ -0,0 +1,8 @@
+public class driveConstants {
+
+    public final static string ID0 = "FrontRightDrive";
+    public final static string ID1 = "FrontLeftDrive";
+    public final static string ID2 = "BackRightDrive";
+    public final static string ID3 = "BackLeftDrive";
+
+}
